ba37c29c35413f2fa4c3c2bdb99d099dda44b9fd
Mohammed Amine Naimi <>
2018-02-23 23:41:03 +0100

    swipe views done

---
+++ app/src/main/java/com/example/amine/nordikhackathonapp/fragmentActivity.java
@@ -0,0 +1,34 @@
+package com.example.amine.nordikhackathonapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+
+
+public class fragmentActivity extends AppCompatActivity {
+
+    // When requested, this adapter returns a DemoObjectFragment,
+    // representing an object in the collection.
+    CollectionDemoActivity.DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
+    ViewPager mViewPager;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection_demo);
+
+        // ViewPager and its adapters use support library
+        // fragments, so use getSupportFragmentManager.
+        mDemoCollectionPagerAdapter =
+                new CollectionDemoActivity.DemoCollectionPagerAdapter(
+                        getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+    }
+
+}
+
